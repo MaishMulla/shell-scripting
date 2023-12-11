215be63edd3221f504c769450e1669ca174deec1
maish <mulla.maish@gmail.com>
2023-12-11 18:47:16 -0500

    new shell

--- 06-special-variables.sh
+++ 06-special-variables.sh
@@ -1,5 +1,13 @@
 #!bin/bash
 echo "special vr are numerical base and special sign"
+a=1000
 echo "print script name : $0 "
 echo "first arguments is $1 "
 echo "second argumemnts is $2 "
+echo "print the value of a is $a"
+echo $0
+echo $*
+echo $*
+echo $@
+echo $#
+echo $$
