aac476bdc0fca9838099dfbc6eab327a3d9757a3
maish <mulla.maish@gmail.com>
2023-12-13 00:34:31 -0500

    new shell

---
+++ 14-ifelse.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+
+echo -e "Demo On If , If Else & Else If Usage"
+
+ACTION=$1 
+
+if [ "$ACTION" == "start" ] ; then 
+    echo -e "\e[32m Starting Shipping Service \e[0m"
+    exit 0
+
+fi 
+
+echo "not meet any condition"
+exit 100
