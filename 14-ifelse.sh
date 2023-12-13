fe1d60b2990bff1669217d313d700e4289022ad3
maish <mulla.maish@gmail.com>
2023-12-13 00:58:17 -0500

    new shell

--- 14-ifelse.sh
+++ 14-ifelse.sh
@@ -7,7 +7,8 @@ ACTION=$1
 if [ "$ACTION" == "start" ] ; then 
     echo -e "\e[33m Starting Shipping Service \e[0m"
     exit 0
-
+else 
+echo -e "\e[34m Starting Shipping Service \e[0m"
 fi 
 
 echo "not meet any condition"
