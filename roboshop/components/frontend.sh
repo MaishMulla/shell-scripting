f7ce15be6367a68a638494c968feb0582adebc95
maish <mulla.maish@gmail.com>
2023-12-16 12:13:05 -0500

    new shell

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-UID=$(id -u)
+USER_UID=$(id -u)
 
-if [ $UID -ne 0 ] ; then 
+if [ $USER_UID -ne 0 ] ; then 
 echo -e " \e[33m this script i would like to run as root user \e[0m"
 echo -e "\e[32m example usages : \n\t\t \e[om sudo bash scriptname componentName"
 exit 1 
