f780d4a87452a6a36f90c4c2482a2f792d4d2069
maish <mulla.maish@gmail.com>
2023-12-16 12:18:42 -0500

    new shell

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 USER_UID=$(id -u)
+COMPONENT=$1
 
 if [ $USER_UID -ne 0 ] ; then 
 echo -e " \e[33m this script i would like to run as root user \e[0m"
@@ -11,13 +12,20 @@ exit 1
 fi
 
 echo "-----configuring frontend-------"
-echo "installing nginx"
+echo -n"installing nginx"
 
 yum install nginx -y     &>> /tmp/frontend.log
 
 if [ $? -eq 0 ] ; then
-echo "success"
+    echo "success"
 else 
-echo "fail"
+    echo "fail"
+fi
+ echo "downloading the component $1"
 
+ # curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+ if [ $? -eq 0 ] ; then
+    echo "success"
+else 
+    echo "fail"
 fi
