385bdcdff20f8ea77f0aa300cc545d48a291ea25
maish <mulla.maish@gmail.com>
2023-12-16 11:35:43 -0500

    new shell

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-UID =(id -u)
 
-if
-[ $UID -ne 0 ] ; then 
+UID=$(id -u)
+
+if [ $UID -ne 0 ] ; then 
 echo -e " \e[33m this script i would like to run as root user \e[0m"
 echo -e "\e[32m example usages : \n\t\t \e[om sudo bash scriptname componentName"
 exit 1 
@@ -14,4 +14,3 @@ echo "-----configuring frontend-------"
 echo "installing nginx"
 
 yum install nginx -y
-systemctl enable nginx
