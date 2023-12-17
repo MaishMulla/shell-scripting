2e133c7e1b38ed1dde07e80ecf213ae3a13b4f1d
maish <mulla.maish@gmail.com>
2023-12-17 00:20:08 -0500

    new shell

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,8 +1,17 @@
 # !/bin/bash
 
 USER_UID=$(id -u)
-COMPONENT=$1
+COMPONENT=frontend
 LOGFILE="/tmp/${COMPOENT}.log"
+
+start()
+{
+    if [ $? -eq 0 ] ; then
+    echo "success"
+else 
+    echo "fail"
+fi
+}
 
 if [ $USER_UID -ne 0 ] ; then 
 echo -e "\e[33m this script i would like to run as root user \e[0m"
@@ -10,62 +19,35 @@ echo -e "\e[32m example usages : \n\t\t \e[om sudo bash scriptname componentName
 exit 1 
 fi
 
-echo "-----configuring $COMPONENT-------"
+echo "-----configuring ${COMPONENT}-------"
 echo -n "installing nginx :"
 
 yum install nginx -y     &>> $LOGFILE
+start $?
 
-if [ $? -eq 0 ] ; then
-    echo "success"
-else 
-    echo "fail"
-fi
- echo -n "downloading the component $COMPONENT :"
-
-  curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
- if [ $? -eq 0 ] ; then
-    echo "success"
-else 
-    echo "fail"
-fi
-
-echo -n "clening the $1 componet :"        
+echo -n "downloading the component $COMPONENT :"
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+start $?
+ 
+echo -n "clening the $COMPONENT :"        
  cd /usr/share/nginx/html
- rm -rf *  &>> /tmp/frontend.log
- if [ $? -eq 0 ] ; then
-    echo "success"
-else 
-    echo "fail"
-fi
+ rm -rf *  &>> $ {LOGFILE}
+ start $?
 
-echo -n "extracting $1: "
-unzip /tmp/frontend.zip     &>> $LOGFILE
-if [ $? -eq 0 ] ; then
-    echo "success"
-else 
-    echo "fail"
-fi
+echo -n "extracting $COMPONENT: "
+unzip -O /tmp/${COMPONET}.zip     &>> $LOGFILE
+start $?
 
 echo -n "configuring $COMPONENT:"
- mv frontend-main/* .
+ mv ${COMPONENT}-main/* .
  mv static/* .
- rm -rf frontend-main README.md
+ rm -rf ${COMPONENT}-main README.md
  mv localhost.conf /etc/nginx/default.d/roboshop.conf
-
-if [ $? -eq 0 ] ; then
-    echo "success"
-else 
-    echo "fail"
-fi
+start $?
 
 echo -n "restarting: $COMPONENT"
 
 systemctl enable nginx      &>> $LOGFILE
 systemctl daemon-reload      &>> $LOGFILE
 systemctl start nginx        &>> $LOGFILE
-
-if [ $? -eq 0 ] ; then
-    echo "success"
-else 
-    echo "fail"
-fi
+start $?
