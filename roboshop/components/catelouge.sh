f6616d296f2de172b3c9e1b4c4f0ecc7ad276797
maish <mulla.maish@gmail.com>
2023-12-18 16:28:43 -0500

    new shell

--- roboshop/components/catelouge.sh
+++ roboshop/components/catelouge.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
 
-echo "configuring catalouge"
+
+USER_UID=$(id -u)
+COMPONENT=catalouge
+LOGFILE="/tmp/${COMPONENT}.log"
+
+stat()
+{
+    if [ $? -eq 0 ] ; then
+    echo "success"
+else 
+    echo "fail"
+fi
+}
+
+if [ $USER_UID -ne 0 ] ; then 
+echo -e "\e[33m this script i would like to run as root user \e[0m"
+echo -e "\e[32m example usages : \n\t\t \e[om sudo bash scriptname componentName"
+exit 1 
+fi
+
+echo -n "configuring Nodejs repo :"
+yum install https://rpm.nodesource.com/pub_16.x/nodistro/repo/nodesource-release-nodistro-1.noarch.rpm -y
+stat $?  
+
+echo -n "installing nodeJS :"
+yum install nodejs -y     &>> $LOGFILE
+stat $?
