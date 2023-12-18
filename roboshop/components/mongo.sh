b9eee3f76909ece93c491023dc8ccec8d3542ae3
maish <mulla.maish@gmail.com>
2023-12-18 15:38:32 -0500

    new shell

--- roboshop/components/mongo.sh
+++ roboshop/components/mongo.sh
@@ -1,7 +1,7 @@
 # !/bin/bash
 
 USER_UID=$(id -u)
-COMPONENT=mongo
+COMPONENT=mongodb
 LOGFILE="/tmp/${COMPONENT}.log"
 MONGO_REPO="https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo"
 SCHEMA_URL="https://github.com/stans-robot-project/mongodb/archive/main.zip"
