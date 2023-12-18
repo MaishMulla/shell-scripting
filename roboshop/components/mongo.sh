7c103f6ecbd458fff57a2a2e08c2e952d6729521
maish <mulla.maish@gmail.com>
2023-12-18 11:17:04 -0500

    new shell

--- roboshop/components/mongo.sh
+++ roboshop/components/mongo.sh
@@ -46,7 +46,7 @@ curl -s -L -o /tmp/mongodb.zip $SCHEMA_URL
 start $?
 
 echo -n "extracting $COMPONENT :"
-unzip -o /tmp/${COMPONENT}.zip   &>> $LOGFILE
+unzip -o /tmp/$COMPONENT.zip   &>> $LOGFILE
 start $?
 
 echo -n "injecting schema :"
