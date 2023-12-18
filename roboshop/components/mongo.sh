e8a77f21a2350892f2911341ca6a47391218807d
maish <mulla.maish@gmail.com>
2023-12-17 20:41:07 -0500

    new shell

---
+++ roboshop/components/mongo.sh
@@ -0,0 +1,33 @@
+# !/bin/bash
+
+USER_UID=$(id -u)
+COMPONENT=mongo
+LOGFILE="/tmp/${COMPOENT}.log"
+MONGO_REPO="https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo"
+
+start()
+{
+    if [ $? -eq 0 ] ; then
+    echo "success"
+else 
+    echo "fail"
+fi
+}
+
+if [ $USER_UID -ne 0 ] ; then 
+echo -e "\e[33m this script i would like to run as root user \e[0m"
+echo -e "\e[32m example usages : \n\t\t \e[om sudo bash scriptname componentName"
+exit 1 
+fi
+
+echo "-----configuring ${COMPONENT}-------"
+
+echo "configuring ${COMPONENT} repo"
+curl -s -o /etc/yum.repos.d/mongodb.repo $MONGO_REPO
+start $?
+
+echo -e "installing $COMPONENT "
+yum install -y mongodb-org &>> ${LOGFILE}
+start $?
+
+
